dfdd13dd45109c3f9246adfd4e23977001aafa03
Aung Ko Ko <aungkokomyintoo@gmail.com>
2019-07-13 18:21:16 +0630

    Profile route and model

--- routes/api/profile.js
+++ routes/api/profile.js
@@ -1,8 +1,24 @@
 const express = require('express');
 const router = express.Router();
+const mongoose = require('mongoose');
+const passport = require('passport');
 
-router.get('/',(req,res) =>{
-    res.send('Profile');
+
+const User = require('../../models/User');
+const Profile = require('../../models/Profile');
+
+router.get('/',passport.authenticate('jwt',{session :false}),(req,res) =>{
+    const errors = {};
+
+    Profile.findOne({user : req.user.id})
+    .then(profile => {
+        if(!profile){
+            errors.noprofile = 'This is no profile for this user';
+            return res.status(404).json(errors)
+        }
+
+        res.json(profile);
+    }).catch(err => res.status(404).json(err));
 });
 
 module.exports = router;
